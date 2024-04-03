67c034d8c87309d51adc9d3296d91a40ebbc4a3a
Thomas Wong <1935201+thomasklwong@users.noreply.github.com>
2024-04-03 21:11:08 +0800

    Update flake for myself
    - Commented out neovim overlay
    - Commented out vim related non-flake
    - Commented out all overlay
    - Disable all outputs nix configuration other than darwin's
    - Copy m1 nix config to my work laptop

--- flake.nix
+++ flake.nix
@@ -27,66 +27,66 @@
     # I think technically you're not supposed to override the nixpkgs
     # used by neovim but recently I had failures if I didn't pin to my
     # own. We can always try to remove that anytime.
-    neovim-nightly-overlay = {
-      url = "github:nix-community/neovim-nightly-overlay";
+    # neovim-nightly-overlay = {
+    #   url = "github:nix-community/neovim-nightly-overlay";
 
-      # Only need unstable until the lpeg fix hits mainline, probably
-      # not very long... can safely switch back for 23.11.
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
+    #   # Only need unstable until the lpeg fix hits mainline, probably
+    #   # not very long... can safely switch back for 23.11.
+    #   inputs.nixpkgs.follows = "nixpkgs-unstable";
+    # };
 
     # Other packages
     zig.url = "github:mitchellh/zig-overlay";
 
     # Non-flakes
-    nvim-conform.url = "github:stevearc/conform.nvim/v5.2.1";
-    nvim-conform.flake = false;
-    nvim-treesitter.url = "github:nvim-treesitter/nvim-treesitter/v0.9.1";
-    nvim-treesitter.flake = false;
-    vim-copilot.url = "github:github/copilot.vim/v1.11.1";
-    vim-copilot.flake = false;
+    # nvim-conform.url = "github:stevearc/conform.nvim/v5.2.1";
+    # nvim-conform.flake = false;
+    # nvim-treesitter.url = "github:nvim-treesitter/nvim-treesitter/v0.9.1";
+    # nvim-treesitter.flake = false;
+    # vim-copilot.url = "github:github/copilot.vim/v1.11.1";
+    # vim-copilot.flake = false;
   };
 
   outputs = { self, nixpkgs, home-manager, darwin, ... }@inputs: let
     # Overlays is the list of overlays we want to apply from flake inputs.
-    overlays = [
-      inputs.neovim-nightly-overlay.overlay
-      inputs.zig.overlays.default
-    ];
+    # overlays = [
+    #   inputs.neovim-nightly-overlay.overlay
+    #   inputs.zig.overlays.default
+    # ];
 
     mkSystem = import ./lib/mksystem.nix {
       inherit overlays nixpkgs inputs;
     };
   in {
-    nixosConfigurations.vm-aarch64 = mkSystem "vm-aarch64" {
-      system = "aarch64-linux";
-      user   = "mitchellh";
-    };
+    # nixosConfigurations.vm-aarch64 = mkSystem "vm-aarch64" {
+    #   system = "aarch64-linux";
+    #   user   = "mitchellh";
+    # };
 
-    nixosConfigurations.vm-aarch64-prl = mkSystem "vm-aarch64-prl" rec {
-      system = "aarch64-linux";
-      user   = "mitchellh";
-    };
+    # nixosConfigurations.vm-aarch64-prl = mkSystem "vm-aarch64-prl" rec {
+    #   system = "aarch64-linux";
+    #   user   = "mitchellh";
+    # };
 
-    nixosConfigurations.vm-aarch64-utm = mkSystem "vm-aarch64-utm" rec {
-      system = "aarch64-linux";
-      user   = "mitchellh";
-    };
+    # nixosConfigurations.vm-aarch64-utm = mkSystem "vm-aarch64-utm" rec {
+    #   system = "aarch64-linux";
+    #   user   = "mitchellh";
+    # };
 
-    nixosConfigurations.vm-intel = mkSystem "vm-intel" rec {
-      system = "x86_64-linux";
-      user   = "mitchellh";
-    };
+    # nixosConfigurations.vm-intel = mkSystem "vm-intel" rec {
+    #   system = "x86_64-linux";
+    #   user   = "mitchellh";
+    # };
 
-    nixosConfigurations.wsl = mkSystem "wsl" {
-      system = "x86_64-linux";
-      user   = "mitchellh";
-      wsl    = true;
-    };
+    # nixosConfigurations.wsl = mkSystem "wsl" {
+    #   system = "x86_64-linux";
+    #   user   = "mitchellh";
+    #   wsl    = true;
+    # };
 
-    darwinConfigurations.macbook-pro-m1 = mkSystem "macbook-pro-m1" {
-      system = "aarch64-darwin";
-      user   = "mitchellh";
+    darwinConfigurations.thomas-wong-work = mkSystem "thomas-wong-work" {
+      system = "x86_64-darwin";
+      user   = "thomas";
       darwin = true;
     };
   };
