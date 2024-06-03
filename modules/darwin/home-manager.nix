c65492b69b1d504f2baabe2d4740ac99714f54ef
Thomas Wong <1935201+thomasklwong@users.noreply.github.com>
2024-06-03 13:36:51 +0800

    Add Dock management

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -81,35 +81,49 @@ in
   };
 
   # Fully declarative dock using the latest from Nix Store
-  # local = { 
-  #   dock = {
-  #     enable = true;
-  #     entries = [
-  #       { path = "/Applications/Slack.app/"; }
-  #       { path = "/System/Applications/Messages.app/"; }
-  #       { path = "/System/Applications/Facetime.app/"; }
-  #       { path = "${pkgs.alacritty}/Applications/Alacritty.app/"; }
-  #       { path = "/System/Applications/Music.app/"; }
-  #       { path = "/System/Applications/News.app/"; }
-  #       { path = "/System/Applications/Photos.app/"; }
-  #       { path = "/System/Applications/Photo Booth.app/"; }
-  #       { path = "/System/Applications/TV.app/"; }
-  #       { path = "/System/Applications/Home.app/"; }
-  #       {
-  #         path = toString myEmacsLauncher;
-  #         section = "others";
-  #       }
-  #       {
-  #         path = "${config.users.users.${user}.home}/.local/share/";
-  #         section = "others";
-  #         options = "--sort name --view grid --display folder";
-  #       }
-  #       {
-  #         path = "${config.users.users.${user}.home}/.local/share/downloads";
-  #         section = "others";
-  #         options = "--sort name --view grid --display stack";
-  #       }
-  #     ];
-  #   };
-  # };
+  local = { 
+    dock = {
+      enable = true;
+      entries = [
+        { path = "/Applications/LINE.app/"; }
+        { path = "/Applications/Signal.app/"; }
+        { path = "/Applications/Slack.app/"; }
+        { path = "/Applications/Floorp.app/"; }
+        { path = "/Applications/Firefox.app/"; }
+        { path = "/Applications/Firefox Developer Edition.app/"; }
+        { path = "/Applications/Google Chrome.app/"; }
+        { path = "/Applications/Google Chrome Beta.app/"; }
+        { path = "/Applications/Google Chrome Canary.app/"; }
+        { path = "/Applications/Brave Browser.app/"; }
+        { path = "/Applications/Brave Browser Beta.app/"; }
+        { path = "/Applications/Microsoft Edge.app/"; }
+        { path = "/Applications/Mullvad Browser.app/"; }
+        { path = "/Applications/Safari.app/"; }
+        { path = "/Applications/1Password.app/"; }
+        { path = "/Applications/Alacritty.app/"; }
+        { path = "/Applications/GitHub Desktop.app/"; }
+        { path = "/Applications/Obsidian.app/"; }
+        { path = "/Applications/Visual Studio Code.app/"; }
+        {
+          path = "${config.users.users.${user}.home}/Downloads";
+          section = "others";
+          options = "--sort name --view grid --display stack";
+        }
+        #{
+        #  path = toString myEmacsLauncher;
+        #  section = "others";
+        #}
+        #{
+        #  path = "${config.users.users.${user}.home}/.local/share/";
+        #  section = "others";
+        #  options = "--sort name --view grid --display folder";
+        #}
+        #{
+        #  path = "${config.users.users.${user}.home}/.local/share/downloads";
+        #  section = "others";
+        #  options = "--sort name --view grid --display stack";
+        #}
+       ];
+     };
+   };
 }
