c7cf0d2abe47d470968966125c0c2a162f050dd8
Thomas Wong <1935201+thomasklwong@users.noreply.github.com>
2026-01-28 13:44:13 +0800

    clean up zsh

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -8,70 +8,58 @@ let name = "Thomas Wong";
   zsh = {
     enable = true;
     autocd = false;
-    cdpath = [ "~/.local/share/src" ];
-    plugins = [
-      # {
-      #    name = "powerlevel10k";
-      #    src = pkgs.zsh-powerlevel10k;
-      #    file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      # }
-      # {
-      #    name = "powerlevel10k-config";
-      #     src = lib.cleanSource ./config;
-      #     file = "p10k.zsh";
-      # }
-    ];
+
+    history = {
+      ignorePatterns = [
+        "pwd"
+        "ls"
+        "cd"
+      ];
+    };
 
     initContent = lib.mkBefore ''
+      # 1. Enable unique constraints on PATH to auto-remove duplicates
+      typeset -U path
+
+      # 2. Load Nix Daemon (if not already loaded by system zshrc)
       if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
         . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
         . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
       fi
 
-      # According to Tauri's doc
-      # Also ANDROID_HOME and NDK_HOME happened after this
+      # 3. Environment Variables
       export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
-
       export ANDROID_HOME=$HOME/Library/Android/sdk
-      export NDK_HOME="$ANDROID_HOME/ndk/$(ls -1 $ANDROID_HOME/ndk)"
 
-      # Define variables for directories
-      
-      # This gives the `avdmanager` and `sdkmanager` command
-      export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin 
+      if [ -d "$ANDROID_HOME/ndk" ]; then
+         export NDK_HOME="$ANDROID_HOME/ndk/$(ls -1 "$ANDROID_HOME/ndk" | head -n 1)"
+      fi
 
-      # This gives the `adb` command
-      export PATH=$ANDROID_HOME/platform-tools:$PATH
-
-      # This gives the `emulator` command
-      export PATH=$ANDROID_HOME/emulator:$PATH
-
-      export PATH=/opt/homebrew/bin:$PATH
-      export PATH=$HOME/.pnpm-packages/bin:$HOME/.pnpm-packages:$PATH
-      export PATH=$HOME/.npm-packages/bin:$HOME/bin:$PATH
-      export PATH=$HOME/.local/share/bin:$PATH
-      export PATH=$HOME/.local/bin:$PATH
-      # This will be first added path
-
-      # Remove history data we don't want to see
-      export HISTIGNORE="pwd:ls:cd"
-
-      # Ripgrep alias
-      alias search="rg -p --glob '!node_modules/*' "  # Added quotes around the alias command
+      # 4. Path Construction (Ordered: Specific -> General)
+      # We explicitly set the path array. $path at the end includes existing system paths.
+      path=(
+        "$HOME/.local/bin"
+        "$HOME/.local/share/bin"
+        "$ANDROID_HOME/emulator"                 # This gives the `emulator` command
+        "$ANDROID_HOME/platform-tools"           # This gives the `adb` command
+        "$ANDROID_HOME/cmdline-tools/latest/bin" # This gives the `avdmanager` and `sdkmanager` command
+        "/opt/homebrew/bin"
+        $path
+      )
 
       # nix shortcuts
       shell() {
           nix-shell '<nixpkgs>' -A "$1"
       }
 
-      # Use difftastic, syntax-aware diffing
-      alias diff=difft
-
-      # Always color ls and group directories
-      alias ls='ls --color=auto'
-
       eval "$(mise activate)"
     '';
+
+    shellAliases = {
+      ls="ls --color=auto";
+      search="rg -p --glob '!node_modules/*' ";
+      diff="difft";
+    };
   };
 
   git = {
